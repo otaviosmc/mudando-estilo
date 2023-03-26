f77bfca2e4450fdb72c97642cdb09da63903a9c1
Otavio <contato.otaviosimoncini@gmail.com>
2023-03-26 13:08:06 -0300

    projeto pronto

--- src/App.js
+++ src/App.js
@@ -3,31 +3,20 @@ import "./App.css";
 import "./index.css";
 
 function App() {
-  const [novoTexto, setNovoTexto] = useState();
+  const [novoEstilo, setNovoEstilo] = useState("w-24 h-24 bg-red-600");
   function mudarEstilo() {
-    setNovoTexto("Feito por Otávio Aparecido Simoncini");
-  }
-  function apagarTexto() {
-    setNovoTexto("");
+    setNovoEstilo("w-24 h-24 bg-blue-600");
   }
   return (
     <div className="bg-gray-200 flex flex-col w-screen h-screen items-center justify-center">
       <div className="bg-white mx-auto p-4 flex justify-center items-center flex-col gap-7">
-        <h1 className="font-bold text-2xl">Inserindo e removendo texto</h1>
-        <div className="bg-blue-600 py-2 px-4 text-white w-11/12 h-12 flex items-center rounded-3xl">
-          <p>{novoTexto}</p>
-        </div>
+        <h1 className="font-bold text-2xl">Mudando estilo</h1>
+        <div className={novoEstilo}></div>
         <button
           className="bg-green-500 py-2 px-4 rounded-2xl"
           onClick={mudarEstilo}
         >
-          Gerar Texto
-        </button>
-        <button
-          className="bg-green-500 py-2 px-4 rounded-2xl"
-          onClick={apagarTexto}
-        >
-          Apagar Texto
+          Mudar Estilo
         </button>
       </div>
     </div>
